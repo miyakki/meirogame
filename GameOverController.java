1cc70f99b1d1a93babf07086ef8d1a4cc47387db
miyakki <miyakki0712@gmail.com>
2025-12-18 12:53:46 +0900

    Add files via upload

---
+++ GameOverController.java
@@ -0,0 +1,21 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class GameOverController {
+
+ @FXML
+ void onGameOverAction(ActionEvent event) {
+  try {
+   StageDB.getGameOverStage().hide();
+   StageDB.getMainSound().stop();
+   
+   StageDB.resetMainStage();
+   StageDB.getMainStage().show();
+   StageDB.getMainStage().setHeight(600);  // ゲーム画面復帰時に明示的にサイズ変更
+   StageDB.getMainStage().setWidth(692);   // ゲーム画面復帰時に明示的にサイズ変更
+   StageDB.getMainSound().play();
+  } catch (Exception ex) {
+   System.out.println(ex.getMessage());
+  }
+ }
+}
